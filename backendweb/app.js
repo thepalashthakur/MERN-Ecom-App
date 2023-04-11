248324d7c9d5bad51b9dbab397bf400e9f217185
Palash Thakur <virtuosophantom@gmail.com>
2023-04-12 01:16:05 +0530

    adding routes for order and products

--- backendweb/app.js
+++ backendweb/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var orderRouter = require('./routes/order');
+var productRouter = require('./routes/product');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/order', orderRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
